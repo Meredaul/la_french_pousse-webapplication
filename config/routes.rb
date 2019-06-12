9ae91bb30c3fff00d4509c6b0281f73fed57f697
Meredaul <meredith.laury@gmail.com>
2019-06-12 10:38:10 +0200

    removed google_client_id & google_client_secret from environments/development && removed routes created for google oauth, because different way when taking into account devise

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,4 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get ‘auth/:provider/callback’, to: ‘sessions#googleAuth’
-  get ‘auth/failure’, to: redirect(‘/’)
 end
