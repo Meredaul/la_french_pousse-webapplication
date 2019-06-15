a776ed2f7b03cbf654954916600a425a35b0fec4
Meredaul <meredith.laury@gmail.com>
2019-06-15 13:48:52 +0200

    add town and coordinates to users && add town to new_session form && sanitize town latitude and longitude in application_controller.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @personnal_town = current_user.town
+    @personnal_town_coordinates = {lat: current_user.latitude, lng: current_user.longitude}
   end
 end
