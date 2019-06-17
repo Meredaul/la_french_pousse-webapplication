5bd7359bd033be6136fdf258e0d06485cde11528
Meredaul <meredith.laury@gmail.com>
2019-06-17 11:38:53 +0200

    yarn add places.js && create javascript/plugins/init_autocomplete.js && call in application.js && set algolia places account

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,3 +5,8 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import { initMapbox } from '../plugins/init_mapbox';
 
 initMapbox();
+
+
+import { initAutocomplete } from '../plugins/init_autocomplete';
+
+initAutocomplete();
---
+++ app/javascript/plugins/init_autocomplete.js
@@ -0,0 +1,14 @@
+import places from 'places.js';
+
+const initAutocomplete = () => {
+  const addressInput = document.getElementById('personnal_registration_town');
+  if (addressInput) {
+    places({
+      container: addressInput,
+//      appId: ENV['ALGOLIA_PLACES_API_ID'],
+//      apiKey: ENV['ALGOLIA_PLACES_API_KEY']
+       });
+  }
+};
+
+export { initAutocomplete };
