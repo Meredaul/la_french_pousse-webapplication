d08ff9075e5f6e4be4f1d70b7624a8a259ff2952
Meredaul <meredith.laury@gmail.com>
2019-06-19 19:38:29 +0200

    seperate markers depending on current_user_personnal_town_marker and companies_around_markers && made 2 different marker styles on the map && centered and zoomed on current_user_personnal_town

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,23 +2,19 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @participations_test = Participation.all
-    @pousses_test = Pouss.all
-    @companies_test = Company.all
-
     current_user_latitude = current_user.latitude
     current_user_longitude = current_user.longitude
 
     @personnal_town_marker = [{ lat: current_user_latitude, lng: current_user_longitude }]
     @companies_around = Company.near([current_user_latitude, current_user_longitude],20, order: :distance)
-#    @citoyens_town_marker = User.all.map{|user| {lat: user.latitude, lng: user.longitude}}
-
     @companies_around_markers = @companies_around.map do |company|
       {
-        lat: company.latitude
-        lng: company.longitude
-        infoWindow: render_to_string(partial: "infoWindow", locals: {company: company})
+        lat: company.latitude,
+        lng: company.longitude,
+        infoWindow: render_to_string(partial: "infoWindow", locals: {company: company}),
+        image_url: helpers.asset_url('logo.png')
       }
     end
+    #    @citoyens_town_marker = User.all.map{|user| {lat: user.latitude, lng: user.longitude}}
   end
 end
--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -3,27 +3,55 @@ import mapboxgl from 'mapbox-gl'
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
 
-  const fitMapToMarkers = (map, markers) => {
+  const fitMapToMarkers = (map, markers, center) => {
     const bounds = new mapboxgl.LngLatBounds();
-    markers.forEach(marker => bounds.extend([marker.lng, marker.lat]));
-    map.fitBounds(bounds, { padding: 70, maxZoom: 10, duration: 0 });
+    markers.forEach((marker) => {
+      bounds.extend([marker.lng, marker.lat]);
+      bounds.extend([2 * center.lng - marker.lng, 2 * center.lat - marker.lat]);
+    });
+    map.fitBounds(bounds, { padding: 50, maxZoom: 14, duration: 0 });
   };
 
   if (mapElement) { // only build a map if there is a dix#map to inject into
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+    const personnal_town_marker = JSON.parse(mapElement.dataset.personnal_town_marker);
+
+    // map
     const map = new mapboxgl.Map( {
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v10',
+      // center: [personnal_town_marker[0].lat, personnal_town_marker[0].lng],
+      zoom: 3,
       attributionControl: false
     });
 
-    const markers = JSON.parse(mapElement.dataset.markers);
-    markers.forEach((marker) => {
-      new mapboxgl.Marker()
+    // seperate current user personnal town marker
+    new mapboxgl.Marker()
+    .setLngLat([ personnal_town_marker[0].lng, personnal_town_marker[0].lat])
+    .addTo(map);
+
+
+    // try seperate company markers
+    const company_around_markers = JSON.parse(mapElement.dataset.company_around_markers);
+    company_around_markers.forEach((marker) => {
+      console.log(marker);
+      const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
+      console.log(marker);
+      const element = document.createElement('div');// Create a HTML element for your custom marker
+      element.className = 'marker';
+      element.style.backgroundImage = `url('${marker.image_url}')`;
+      element.style.backgroundSize = 'contain';
+      element.style.backgroundRepeat = 'no-repeat';
+      element.style.width = '25px';
+      element.style.height = '25px';
+
+      new mapboxgl.Marker(element)// Pass the element as an argument to the new marker
         .setLngLat([ marker.lng, marker.lat])
+        .setPopup(popup)
         .addTo(map);
     });
-    fitMapToMarkers(map, markers);
+
+    fitMapToMarkers(map, company_around_markers, personnal_town_marker[0]);
   }
 };
 
