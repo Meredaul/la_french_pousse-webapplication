eb11f9a44b0e9bf8cc5eeaa13482556845c0a5bc
Meredaul <meredith.laury@gmail.com>
2019-07-17 00:39:23 +0200

    adjust seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(email: "laury@lafrenchpousse.com", first_name: "Laury", last_name: "Meredith", photo: "image/upload/v1563213725/ogitj3jijuc8ejfpwhr4.png", town: nil, latitude: 50.5672807, longitude: 2.7848217, address: "13 Rue du Bas Chemin, Lorgies, Hauts-de-France, Fr...", password: "patati")
-
 Category.create(name: "Fruits & Légumes", icon: 'fruits_et_legumes.png')
 Category.create(name: "Miel", icon: 'miel.png')
 Category.create(name: "Vin", icon: 'vin.png')
 Category.create(name: "Lait", icon: 'lait.png')
 Category.create(name: "Fromage", icon: 'fromage.png')
 Category.create(name: "Viande", icon: 'viande.png')
-
