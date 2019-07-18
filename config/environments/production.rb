ca9e98f3cbad890035ff7a799d310cdfd792f3b0
Meredaul <meredith.laury@gmail.com>
2019-07-18 18:09:50 +0200

    changed wss to ws (no https) in config/cable.yml

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.action_cable.url = 'wss://www.lafrenchpousse.top/cable'
+  config.action_cable.url = 'ws://www.lafrenchpousse.top/cable'
   config.action_cable.allowed_request_origins = [ "http://www.lafrenchpousse.top", "https://www.lafrenchpousse.top" ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
